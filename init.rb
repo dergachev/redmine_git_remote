25180d09091e0aab52c03e539674319024d27e53
flozzone <flozzone@gmail.com>
2020-05-08 18:22:21 +0200

    Make repository clone path configurable via settings.
    Signed-off-by: flozzone <flozzone@gmail.com>

--- init.rb
+++ init.rb
@@ -9,4 +9,8 @@ Redmine::Plugin.register :redmine_git_remote do
   url 'https://github.com/dergachev/redmine_git_remote'
   description 'Automatically clone and fetch remote git repositories'
   version '0.0.1'
+
+  settings :default => {
+    'git_remote_repo_clone_path' => Pathname.new(__FILE__).join("../").realpath.to_s + "/repos",
+  }, :partial => 'settings/git_remote_settings'
 end
